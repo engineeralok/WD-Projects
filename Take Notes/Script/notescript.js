7d8d02a83238904eb8d3b7d6b1243d096cf308a2
engineeralok <rebelku1@gmail.com>
2021-07-05 18:52:09 +0530

    notescript.js modified

--- Take Notes/Script/notescript.js
+++ Take Notes/Script/notescript.js
@@ -1,1 +1,19 @@
 console.log("hello World");
+
+// if user add a note following code will store it in localStorage. 
+let addBtn = document.getElementById('addBtn');
+
+addBtn.addEventListener('click',function(e) {
+    let addTxt = document.getElementById('addTxt');
+    let notes = localStorage.getItem("notes");
+    if(notes == null){
+        notesObj = [];
+    }
+    else{
+        notesObj = JSON.parse(notes);
+    }
+    notesObj.push(addTxt.value);
+    localStorage.setItem("notes",JSON.stringify(notes))
+    addTxt.value = "";
+    console.log(notesObj)
+})
